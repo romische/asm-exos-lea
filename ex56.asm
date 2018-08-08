1cc8e81e09170f57a7334536e46f8c38a529a7cb
Romische <romane.schelkens@gmail.com>
2018-08-08 17:08:59 +0200

    ex56

--- ex56.asm
+++ ex56.asm
@@ -2,7 +2,7 @@
 
 SECTION .data
         N DW 10
-	      V DW 3, 10, 5, 15, 4, 17, 1, 9, 2, 19
+	V DW 3, 10, 5, 15, 4, 17, 1, 9, 2, 19
         W DW 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 section .text
@@ -11,32 +11,45 @@ CMAIN:
     mov ebp, esp         ; for correct debugging
     
     ;initialisation registres
-    mov ecx, [N]
-    mov edx, 0
-    mov eax, [V]
+    mov eax, 0      ; init à 0 pour éviter segmentation fault
+    mov ebx, 0
+    mov ecx, 0
+    mov cx, [N]
+    mov edx, 0      ; indice de la position
     jmp position
 
                 
 position:
-    ;
-    ;
-    ;
-    ;
-    ;
+    mov ax, [V+edx]
+    mov [W+(eax-1)*4], ax    ;place position dans le nouveau vecteur
     jmp value
 
         
 value:
-    add edx, 2
+    add edx, 2              ; indice de la valeur
     dec ecx
-    mov eax, [V+edx]
-    mov [W+edx], [V+edx]    ;place value dans le nouveau vecteur
-    add edx, 2
+    mov bx, [V+edx]    
+    mov [W+((eax-1)*4)+2], bx   ;place value dans le nouveau vecteur
+    add edx, 2              ; indice de la position suivante
     dec ecx
+    ; continue if ecx!=0
     cmp ecx, 0
-    jz end
+    jz printvect
     jmp position
+    
      
+printvect:
+     mov edx, 0
+     mov cx, [N]
+     
+print_loop:
+     mov ax, [W+edx*2]
+     PRINT_UDEC 2, ax
+     NEWLINE
+     inc edx
+     cmp ecx, edx
+     je end
+     jmp print_loop
 
 end:
     ret
