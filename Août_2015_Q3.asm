79b51dd95513f02f40907c5140980ccb45d40ec6
llepoint <32895801+llepoint@users.noreply.github.com>
2018-08-16 03:13:20 +0200

    Update Août_2015_Q3.asm

--- Août_2015_Q3.asm
+++ Août_2015_Q3.asm
@@ -1,34 +1,41 @@
 %include "io.inc"
 
-section .data
-        N DD 5
-        V DD 1, 5, 2, 0, 4
-        K DD 0
-
-
 section .text
 global CMAIN
 CMAIN:
 
 val:
-    push ebp
+    push ebp					
     mov ebp, esp
-    push eax
     push ebx
     push ecx
-    mov eax, [V]
-    mov ebx, [N]
-    mov ecx, [K]
-    
-    
-    
+    push edx
+ 
+    mov ecx, [ebp+8]        ;1er parametre: V
+    mov ebx, [ebp+12]       ;2eme parametre: N
+    mov edx, [ebp+16]       ;3eme parametre: K
+    cmp edx, ebx            ;comparaison k < n
+    ja bcle                 ;if k < n, jump bcle
+    mov eax, ecx
+    jmp end
+
+bcle:
+    mov eax, edx
+    mul 2                   ;low in eax, high in edx
+    add eax, 1              ;pas inc car carry not affected
+    adc edx, 0
+    push ebx
+    push ecx
+    push edx
     call val
-    
-    
-    
-    
+    pop edx
     pop ecx
     pop ebx
-    pop eax
-    
+
+end:
+    pop edx
+    pop ecx
+    pop ebx
+    mov esp, ebp
+    pop ebp
     ret
